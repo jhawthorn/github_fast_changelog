06709adb9062fe6af8e81986a4559071f723329c
John Hawthorn <john@hawthorn.email>
2018-05-14 14:35:10 -0700

    Initial working version

--- github_fast_changelog.gemspec
+++ github_fast_changelog.gemspec
@@ -9,19 +9,10 @@ Gem::Specification.new do |spec|
   spec.authors       = ["John Hawthorn"]
   spec.email         = ["john@hawthorn.email"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Generates a CHANGELOG based on GitHub PRs}
+  spec.description   = %q{Uses GitHub's v4 GraphQL API to generate a CHANGELOG from PRs matching a list of commits}
+  spec.homepage      = "https://github.com/jhawthorn/github_fast_changelog"
   spec.license       = "MIT"
-
-  # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
-  # to allow pushing to a single host or delete this section to allow pushing to any host.
-  if spec.respond_to?(:metadata)
-    spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
-  else
-    raise "RubyGems 2.0 or newer is required to protect against " \
-      "public gem pushes."
-  end
 
   spec.files         = `git ls-files -z`.split("\x0").reject do |f|
     f.match(%r{^(test|spec|features)/})
@@ -29,6 +20,9 @@ Gem::Specification.new do |spec|
   spec.bindir        = "exe"
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
+
+  spec.add_dependency "graphql", "~> 1.6"
+  spec.add_dependency "graphql-client"
 
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
--- lib/github_fast_changelog.rb
+++ lib/github_fast_changelog.rb
@@ -1,5 +1,70 @@
+require 'graphql/client'
+require 'graphql/client/http'
+
 require "github_fast_changelog/version"
 
 module GithubFastChangelog
-  # Your code goes here...
+  HTTP = GraphQL::Client::HTTP.new("https://api.github.com/graphql") do
+    def headers(context)
+      if token = ENV['GITHUB_ACCESS_TOKEN']
+        { "Authorization" => "Bearer #{token}" }
+      else
+        fail "Missing ENV GITHUB_ACCESS_TOKEN"
+      end
+    end
+  end
+  Schema = GraphQL::Client.load_schema(HTTP)
+  Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+
+  MergedPullsQuery = Client::Client.parse <<-GRAPHQL
+    query($before: String, $owner: String!, $name: String!) {
+      repository(owner: $owner, name: $name) {
+        name
+        pullRequests(last:100, before: $before, states:MERGED) {
+          pageInfo {
+            endCursor
+            hasNextPage
+            hasPreviousPage
+            startCursor
+          }
+          edges {
+            node {
+              id
+              number
+              title
+              url
+              author {
+                login
+                url
+              }
+              mergedAt
+              mergeCommit {
+                oid
+              }
+            }
+          }
+        }
+      }
+    }
+  GRAPHQL
+
+  def each_pull_request(owner:, name:)
+    cursor = nil
+    loop do
+      result = Client.query(MergedPullsQuery, variables: {
+        before: cursor,
+        owner: owner,
+        name: name
+      })
+      data = result.data.repository.pull_requests
+      more = data.page_info.has_previous_page
+      cursor = data.page_info.start_cursor
+
+      data.edges.reverse.map do |edge|
+        yield edge.node
+      end
+
+      break unless more
+    end
+  end
 end
